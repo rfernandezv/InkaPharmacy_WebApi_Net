3277dcb605ec3e66eb63349de5c8dcf182674dfe
Jhonatan Tirado <jhonatantiradotiradodeep@gmail.com>
2018-12-05 20:15:58 -0500

    Login using POST

---
+++ Api/Security/Application/Dto/UserCredentialsDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace InkaPharmacy.Api.Security.Application.Dto
+{
+    public class UserCredentialsDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Api/Security/Controllers/SecurityController.cs
+++ Api/Security/Controllers/SecurityController.cs
@@ -15,6 +15,7 @@ using InkaPharmacy.Api.Common.Constants;
 using InkaPharmacy.Api.Employees.Domain.Entity;
 using InkaPharmacy.Api.Employees.Application.Dto;
 using InkaPharmacy.Api.Employees.Application.Assembler;
+using InkaPharmacy.Api.Security.Application.Dto;
 
 namespace InkaPharmacy.Api.Controllers
 {
@@ -76,6 +77,44 @@ namespace InkaPharmacy.Api.Controllers
             }
         }
 
+        [HttpPost]
+        public IActionResult Login(UserCredentialsDTO userCredentialsDTO)
+        {
+            if (userCredentialsDTO == null)
+                throw new ArgumentException("Credentials are missing");
+
+            bool uowStatus = false;
+            try
+            {
+                Specification<Employee> specification = GetLogingSpecification(userCredentialsDTO.Username, userCredentialsDTO.Password);
+                uowStatus = _unitOfWork.BeginTransaction();
+                List<Employee> employees = _securityRepository.GetList(specification);
+                _unitOfWork.Commit(uowStatus);
+
+                if (employees.FirstOrDefault() == null)
+                {
+                    throw new ArgumentException("Employee is not logged in");
+                }
+
+                EmployeeDto employeeDto = _empleadoLoginAssembler.toDto(employees.FirstOrDefault());
+                var token = GenerateToken(employeeDto.Username);
+                return Ok(this.responseHandler.getOkCommandResponse("bearer " + token, Constants.HttpStatus.Success, employeeDto));
+
+            }
+            catch (ArgumentException ex)
+            {
+                _unitOfWork.Rollback(uowStatus);
+                Console.WriteLine(ex.StackTrace);
+                return Unauthorized();
+            }
+            catch (Exception ex)
+            {
+                _unitOfWork.Rollback(uowStatus);
+                Console.WriteLine(ex.StackTrace);
+                return StatusCode(StatusCodes.Status500InternalServerError, this.responseHandler.getAppExceptionResponse());
+
+            }
+        }
         private Specification<Employee> GetLogingSpecification(string usu, string clave )
         {
             Specification<Employee> specification = Specification<Employee>.All;
