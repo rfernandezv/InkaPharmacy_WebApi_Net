74d43d88c6a2d81343e9f48ec79852b0927a617a
Jhonatan Tirado <jhonatantiradotiradodeep@gmail.com>
2018-12-07 22:57:11 -0500

    Remove password from EmployeeDTO

--- Api/Employee/Application/Dto/EmployeeDto.cs
+++ Api/Employee/Application/Dto/EmployeeDto.cs
@@ -11,7 +11,6 @@
         public  long Role_id { get; set; }
         public  long Store_id { get; set; }
         public  string Username { get; set; }
-        public  string Password { get; set; }
         public  string Email { get; set; }
         public  int Status { get; set; }
 
