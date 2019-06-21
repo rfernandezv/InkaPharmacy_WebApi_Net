2fe9138458acb49a4e2437d68e3995c32dc2edf5
Jhonatan Tirado <jhonatantiradotiradodeep@gmail.com>
2019-06-21 00:30:12 -0500

    IN-15 #comment #close Move GenerateToken to Common. Clean code, using and namespaces

---
+++ Api/Common/Application/Security/Token.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace InkaPharmacy.Api.Common.Application.Security
+{
+    public class Token
+    {
+        public static string GenerateToken(string username)
+        {
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(JwtRegisteredClaimNames.Nbf, new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString()),
+                new Claim(JwtRegisteredClaimNames.Exp, new DateTimeOffset(DateTime.Now.AddDays(1)).ToUnixTimeSeconds().ToString()),
+            };
+
+            var TokenSecret = Environment.GetEnvironmentVariable("InkaPharmacyTokenSecret");
+            Console.WriteLine(TokenSecret);
+
+            var token = new JwtSecurityToken(
+                new JwtHeader(new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(TokenSecret)),
+                                             SecurityAlgorithms.HmacSha256)),
+                    new JwtPayload(claims));
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
